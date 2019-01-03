529e2f00e89106ade2a9a485b9c652042ae7e02a
Bethan Perkins <bethanmary@gmail.com>
2019-01-03 19:52:58 +0000

    Updated with quadratic-equation based derivation of scale and offset.

--- scale_and_offset.py
+++ scale_and_offset.py
@@ -17,33 +17,40 @@ class ScaleOffsetCalculator:
         """
 
         # Mask out missing values
-        fill_mask = data.values == fill_value
+        fill_mask = data == fill_value
 
         # Get the max/min range of the final dataset. This is 90% of the range
         # available in the datatype
         dtype_range = np.iinfo(dtype).max - np.iinfo(dtype).min
-        output_min = np.iinfo(dtype).min + dtype_range*0.05
+        target_max = np.iinfo(dtype).min + dtype_range * 0.95
+        target_min = np.iinfo(dtype).min + dtype_range*0.05
 
         # Range of the passed data
-        input_range = data[~fill_mask].max() - data[~fill_mask].min()
+        input_max = data[~fill_mask].max()
         input_min = data[~fill_mask].min()
 
-        # Calculate offset required to bring the minimum data value to the
-        # minimum output value
-        offset = input_min - output_min
+        # Calculate scale factor
+        scale_factor = (input_min - input_max) / (target_min - target_max)
 
-        # Inversley apply the offset
-        intermediate_data = data - offset
+        # Calculate offset
+        offset = input_max - (target_max * scale_factor)
 
-        # Calculate the scale factor required to bring the data range to 90%
-        # of the dtype range
-        scale_factor = input_range / (dtype_range * 0.9)
+        # Cut both scale and offset down to 2 sig figures to avoid
+        # over-specifying from small values
+        scale_factor = float(('%.1e') % scale_factor)
+        offset = float(('%.1e') % offset)
 
-        # Apply the scaling factor
-        output_data = intermediate_data / scale_factor
+        # Inversly apply scale and offset
+        output_data = (data - offset) / scale_factor
 
-        # Re-apply the fill value
-        output_data[fill_mask] = fill_value
+        # Conver to datatype
+        output = output_data.astype(dtype)
 
-        return output_data, scale_factor, offset
+        return output, scale_factor, offset
 
+
+if __name__ == "__main__":
+
+    soc = ScaleOffsetCalculator()
+
+    soc.get(np.array([0, 100, 200]), 999)
